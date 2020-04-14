c7456c83f360b7afa66a9e5166880a5f504f368c
kmolLin <pyquino@gmail.com>
2020-04-14 13:15:46 +0800

    modify the ui and add the count

--- core/mainwindow.py
+++ core/mainwindow.py
@@ -3,6 +3,7 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
 
 from collections import deque
 from .webcam import Webcam
@@ -10,6 +11,7 @@ from .VideoWidget import VideoWidget
 
 from .calibation import Dialog
 
+import platform
 import numpy as np
 import datetime
 import cv2
@@ -22,42 +24,45 @@ greenLower = (100, 43, 46)
 greenUpper = (124, 255, 255)
 pts = deque(maxlen=64)
 
-#800*600 pixel
-mtx  = np.array(([915.87877768, 0.00000000e+00,380.14276358], 
-                      [0.00000000e+00,915.88537288, 314.01035384],
-                     [0.00000000e+00, 0.00000000e+00, 1.00000000e+00] ))
-                        
+# 800*600 pixel
+mtx = np.array(([915.87877768, 0.00000000e+00, 380.14276358],
+                [0.00000000e+00, 915.88537288, 314.01035384],
+                [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]))
 
-dist = np.array(([0.02764797, -0.33807616 , 0.00095279  ,-0.0024703,  0.57549135]))
-  
+dist = np.array(([0.02764797, -0.33807616, 0.00095279, -0.0024703, 0.57549135]))
 
-class MainWindow(QMainWindow, Ui_MainWindow):
+
+class MainWindow(QMainWindow):
 
     def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.setupUi(self)
+        super(MainWindow, self).__init__()
+        loadUi("core/mainwindow.ui", self)
+        self.show()
         self.running = False
         self.binary_toggle = False
-        
+
         self.video_stream = None
         self.timers = list()
         self.VidFrameGUI = self.VidFrame
         self.VidFrame = VideoWidget(self.VidFrame)
-        
+
         parseImage(self)
         self.startButton.clicked.connect(self.start_clicked)
         self.showBinaryButton.clicked.connect(self.binary_clicked)
-        self.generate.clicked.connect(self.generatearray)
-        
-        #self.generteMatrix =0
+
+        # self.generteMatrix =0
 
         self.setup_timers()
         self.startButton.setFocus()
-        
+        self.model = QStringListModel(self)
+        toolbox = ["threshold", "Canny", "Houghline", "morphologyEx", "crop image", "dilate", "findContours"]
+        # self.model.setStringList(li)
+        self.image_box.addItems(toolbox)
+        self.listView.setModel(self.model)
+
     def update_time(self):
         self.currTimeLabel.setText(datetime.datetime.now().strftime('%I:%M:%S %p'))
         self.currDateLabel.setText(datetime.datetime.now().strftime('%d-%b-%Y'))
-        
 
     def setup_timers(self):
         timer1 = QTimer(self)
@@ -91,44 +96,67 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.showBinaryButton.setText('Show Color')
         if not self.binary_toggle:
             self.showBinaryButton.setText('Show Binary')
-            
+
     def __openDlg__(self):
         dlg2 = Dialog()
         dlg2.show()
         if dlg2.exec_(): pass
 
-    def InConvertImage(self, frame1):
-        """ the convert will convert inner matrix to the frame.
-        """
-        h,  w = frame1.shape[:2]
-        newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-        # this is second method in the conver Image.
-        #dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)  method 1
-        mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
-        dst = cv2.remap(frame1,mapx,mapy,cv2.INTER_LINEAR)
-        x,y,w,h = roi
-        dst = dst[y:y+h, x:x+w]
-        return dst
+    @pyqtSlot()
+    def on_add_list_view_clicked(self):
+
+        row = self.listView.currentIndex().row()
+        print(row)
+        if row == -1:
+            row = 0
+        self.model.insertRow(row)
+        index = self.model.index(row)
+
+        name = self.image_box.currentText()
+        self.model.setData(index, f"{name}")
+        # self.model.setData(0)
+
+
+    @pyqtSlot()
+    def on_insert_list_view_clicked(self):
+        self.image_box.count()
+
+    @pyqtSlot()
+    def on_delete_list_view_clicked(self):
+        print("456")
+
+    # def InConvertImage(self, frame1):
+    #     """ the convert will convert inner matrix to the frame.
+    #     """
+    #     h, w = frame1.shape[:2]
+    #     newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+    #     # this is second method in the conver Image.
+    #     # dst = cv2.undistort(frame, mtx, dist, None, newcameramtx)  method 1
+    #     mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w, h), 5)
+    #     dst = cv2.remap(frame1, mapx, mapy, cv2.INTER_LINEAR)
+    #     x, y, w, h = roi
+    #     dst = dst[y:y + h, x:x + w]
+    #     return dst
 
     def update_frame(self):
         if self.running:
             frame = self.video_stream.read()
-            
+
             if self.binary_toggle:
-                #(grabbed, frame) = self.video_stream.read()
-                frame = self.InConvertImage(frame)
-                
+                # (grabbed, frame) = self.video_stream.read()
+                frame = frame
+
                 self.trackObject(frame)
 
-                #frame = self.clean_img(frame)
-            
+                # frame = self.clean_img(frame)
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame_height, frame_width, _ = frame.shape
             frame = QImage(frame.data,
-                                 frame_width,
-                                 frame_height,
-                                 frame.strides[0],
-                                 QImage.Format_RGB888)
+                           frame_width,
+                           frame_height,
+                           frame.strides[0],
+                           QImage.Format_RGB888)
             self.VidFrame.setImage(frame)
 
     def clean_img(self, img):
@@ -139,7 +167,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         clean = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
         clean = cv2.merge((clean, clean, clean))
         return clean
-        
+
     def loadimage(self, name='buffer.png'):
         self.generatearray()
 
@@ -149,30 +177,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             | r_12  r_22  r_32 |    ||    | Ty |
             | r_13  r_23  r_33 |    ||    | Tz |
         """
-        Zc = 112 #self.Zc.value()
+        Zc = 112  # self.Zc.value()
         Zw = 1030
-        
-        world = np.array( 
-        [[[self.gp1x.value()],[self.gp1y.value()],[Zc]],
-        [[self.gp2x.value()],[self.gp2y.value() ],[Zc]],
-        [[self.gp3x.value()],[self.gp3y.value()],[Zc]],
-        [[self.gp4x.value()],[self.gp4y.value()],[Zc]]])
+
+        world = np.array(
+            [[[self.gp1x.value()], [self.gp1y.value()], [Zc]],
+             [[self.gp2x.value()], [self.gp2y.value()], [Zc]],
+             [[self.gp3x.value()], [self.gp3y.value()], [Zc]],
+             [[self.gp4x.value()], [self.gp4y.value()], [Zc]]])
 
         pixmat = np.array(
-        [[[self.pp1x.value()],[self.pp1y.value()]],
-       [[self.pp2x.value()],[self.pp2y.value()]],
-       [[self.pp3x.value()],[self.pp3y.value()]],
-       [[self.pp4x.value()],[self.pp4y.value()]]])
-        ret ,  rvec, self.tevc = cv2.solvePnP(world, pixmat, mtx, dist)
-        scriz = np.array([[0,0,0,1]])
+            [[[self.pp1x.value()], [self.pp1y.value()]],
+             [[self.pp2x.value()], [self.pp2y.value()]],
+             [[self.pp3x.value()], [self.pp3y.value()]],
+             [[self.pp4x.value()], [self.pp4y.value()]]])
+        ret, rvec, self.tevc = cv2.solvePnP(world, pixmat, mtx, dist)
+        scriz = np.array([[0, 0, 0, 1]])
         self.rotation, b = cv2.Rodrigues(rvec)
-        self.aamat = np.c_[self.rotation,self.tevc]
-    
+        self.aamat = np.c_[self.rotation, self.tevc]
+
     def checkpoint(self):
         Zc = 1238
         check = np.array([[self.inputpixelX.value()], [self.inputpixelY.value()], [1]])
-        endpoint = np.dot(np.linalg.inv(np.dot(mtx, self.rotation)),( Zc*check-np.dot(mtx, self.tevc)))
+        endpoint = np.dot(np.linalg.inv(np.dot(mtx, self.rotation)), (Zc * check - np.dot(mtx, self.tevc)))
         print(endpoint)
+
     """
     def testpoint(self):
         Zc = 1238
@@ -187,23 +216,23 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         print("*"*10)
         print(endpixel)
     """
+
     def calctrackobject(self, image):
-        """tracking max rect object 
+        """tracking max rect object
         """
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, greenLower, greenUpper)
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]
+                                cv2.CHAIN_APPROX_SIMPLE)[-2]
         center = None
         if len(cnts) > 0:
             c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            print("{}\t{}".format(center[0],center[1] ))
-        
+            print("{}\t{}".format(center[0], center[1]))
 
     def clear_timers(self):
         while self.timers:
@@ -217,7 +246,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.video_stream.stop()
         self.close()
         sys.exit()
-    
+
     def catchimage(self):
         """ Save capcture image
         """
@@ -226,7 +255,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         file = "buffer.png"
         cv2.imwrite(file, camera_capture)
         self.loadimage()
-        
+
     def trackObject(self, frame1):
         """ tracking object function
         """
@@ -235,29 +264,29 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)[-2]
-        
+                                cv2.CHAIN_APPROX_SIMPLE)[-2]
+
         center = None
         if len(cnts) > 0:
             c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            #print("{}\t{}".format(center[0],center[1] ))
-            self.trackX.setText(str(center[0]))
-            self.trackY.setText(str(center[1]))
+            # print("{}\t{}".format(center[0],center[1] ))
+            # self.trackX.setText(str(center[0]))
+            # self.trackY.setText(str(center[1]))
             if radius > 10:
                 cv2.circle(frame1, (int(x), int(y)), int(radius),
-                (0, 255, 255), 2)
+                           (0, 255, 255), 2)
             cv2.circle(frame1, center, 5, (0, 0, 255), -1)
-            
+
         pts.appendleft(center)
         for i in range(1, len(pts)):
             if pts[i - 1] is None or pts[i] is None:
                 continue
-            thickness = int(np.sqrt(64/ float(i + 1)) * 2.5)
+            thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
             cv2.line(frame1, pts[i - 1], pts[i], (0, 0, 255), thickness)
-            
+
     def outputGenerte(self, matrix):
         """ Save the Generate matrix to .txt
         """
@@ -273,54 +302,16 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 data2.append(str(listdata[2][e]))
             print(data2)
             with open(filename, 'w') as f:
-                f.write(str(data+'\n'))
-                f.write(str(data1+'\n'))
-                f.write(str(data2+'\n'))
-        
-    @pyqtSlot()
-    def on_load_clicked(self):
-        filename, _ = QFileDialog.getOpenFileName(self, "Open File", ".", "Text files (*.txt)")
-        if filename:
-            with open(filename, 'r') as f:
-                read_list = eval(f.read())
-            for (x, y), (spinbox_x, spinbox_y) in zip(read_list, (
-                (self.gp1x, self.gp1y),
-                (self.gp2x, self.gp2y),
-                (self.gp3x, self.gp3y),
-                (self.gp4x, self.gp4y),
-                (self.pp1x, self.pp1y),
-                (self.pp2x, self.pp2y),
-                (self.pp3x, self.pp3y),
-                (self.pp4x, self.pp4y)
-            )):
-                spinbox_x.setValue(x)
-                spinbox_y.setValue(y)
-    
-    @pyqtSlot()
-    def on_output_clicked(self):
-        filename, _ = QFileDialog.getSaveFileName(self, "Save File", "./test.txt", "Text files (*.txt)")
-        if filename:
-            data = []
-            for spinbox_x, spinbox_y in (
-                (self.gp1x, self.gp1y),
-                (self.gp2x, self.gp2y),
-                (self.gp3x, self.gp3y),
-                (self.gp4x, self.gp4y),
-                (self.pp1x, self.pp1y),
-                (self.pp2x, self.pp2y),
-                (self.pp3x, self.pp3y),
-                (self.pp4x, self.pp4y)
-            ):
-                data.append((spinbox_x.value(), spinbox_y.value()))
-            with open(filename, 'w') as f:
-                f.write(str(data))
-    
+                f.write(str(data + '\n'))
+                f.write(str(data1 + '\n'))
+                f.write(str(data2 + '\n'))
+
     @pyqtSlot()
     def on_getpixel_clicked(self):
-        # for testing code 
-        #self.testpoint()
+        # for testing code
+        # self.testpoint()
         self.checkpoint()
-    
+
     @pyqtSlot()
     def on_saveImage_clicked(self):
         """
@@ -331,9 +322,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if filename:
             camera_capture = self.video_stream.saveimage()
             cv2.imwrite(filename, camera_capture)
-        
-    
+
     @pyqtSlot()
     def on_actioncalibation_triggered(self):
         self.__openDlg__()
-
--- launch.py
+++ launch.py
@@ -1,9 +1,10 @@
-##Start opencv
-##Copyright (C) 2017 you shang [pyquino@gmail.com]
+# Start opencv
+# Copyright (C) 2017 you shang [pyquino@gmail.com]
 from core.mainwindow import MainWindow
 from PyQt5.QtWidgets import QApplication
 from sys import argv, exit
-if __name__=='__main__':
+
+if __name__ == '__main__':
     QApplication.setStyle('fusion')
     app = QApplication(argv)
     run = MainWindow()
